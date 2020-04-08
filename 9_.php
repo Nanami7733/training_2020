b4f4bf8cb35b17174f04e37dd40b118defaf7f2a
“Nanami <n.ishihara@dreamcareer.co.jp>
2020-04-08 17:17:17 +0900

    9-7追加

--- 9_.php
+++ 9_.php
@@ -113,4 +113,90 @@ foreach ($players as $player) {
 
 
 
+
+//9-6
+// クラスからメソッドを呼び出してみよう
+class Player {
+    public $myName;
+
+    public function __construct($name) {
+        $this->myName = $name;
+    }
+
+    public function attack($enemy) {
+        echo $this->myName."は、".$enemy."を攻撃した！\n";
+    }
+}
+
+class Wizard extends Player {
+    public function __construct(){
+        parent::__construct("魔法使い");
+    }
+    public function attack($enemy) {
+        $this -> spell();
+        echo $this->myName."は、".$enemy."に炎を放った！\n";
+    }
+    private function spell(){
+        echo "シャラララーン！\n";
+    }
+}
+
+echo "=== パーティーでスライムと戦う ===\n";
+$hero = new Player("勇者");
+// $hero->attack("スライム");
+$warrior = new Player("戦士");
+$wizard = new Wizard();
+
+$party = [$hero, $warrior, $wizard];
+foreach ($party as $member) {
+    $member->attack("スライム");
+}
+
+
+
+//9-7
+//  クラス変数とクラスメソッド
+class Player {
+    public $myName;
+    private static $charactorCount = 0;
+
+    public function __construct($name) {
+        $this->myName = $name;
+        Player::$charactorCount++;
+        echo Player::$charactorCount."番目のプレイヤー、".$this->myName."が登場した。\n";
+    }
+    public static function charactorCount() {
+        return Player::$charactorCount;
+    }
+    public function attack($enemy) {
+        echo $this->myName."は、".$enemy."を攻撃した！\n";
+    }
+}
+
+class Wizard extends Player {
+    public function __construct() {
+        parent::__construct("魔法使い");
+    }
+    private function spell() {
+        echo "シャラララーン\n";
+    }
+    public function attack($enemy) {
+        $this->spell();
+        echo $this->myName."は、".$enemy."に炎を放った！\n";
+    }
+}
+
+echo "=== パーティーでスライムと戦う ===\n";
+$hero    = new Player("勇者");
+// $hero->attack("スライム");
+$warrior = new Player("戦士");
+$wizard = new Wizard();
+$party   = [$hero, $warrior, $wizard];
+
+foreach ($party as $player) {
+     $player->attack("スライム");
+}
+// $wizard->spell();
+echo Player::charactorCount()."人で、スライムを攻撃した";
+
 ?>
