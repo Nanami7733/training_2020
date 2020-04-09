d3abed22c8a82cc42a19d16a41f586c881580048
“Nanami <n.ishihara@dreamcareer.co.jp>
2020-04-09 10:41:08 +0900

    10-2完了

---
+++ paiza_10.php
@@ -0,0 +1,17 @@
+<?php
+//10-2-1
+// 例外処理を定義しよう
+
+echo "start\n";
+try{
+    $date = new DateTime("199x-01-01");
+    echo $date->format('Y/m/d') . "\n";
+}catch(Exception $e){
+    echo $e -> getMessage()."\n";
+}finally{
+    echo "end\n";
+}
+
+
+
+?>
