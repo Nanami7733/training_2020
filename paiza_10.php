6e3c1607df3d6e5ef9c7190381261a95b4eb5b62
“Nanami <n.ishihara@dreamcareer.co.jp>
2020-04-09 13:36:52 +0900

    10-7完了

--- paiza_10.php
+++ paiza_10.php
@@ -13,5 +13,104 @@ try{
 }
 
 
+//10-3 出力とは別の例外メッセージ
+echo "start\n";
+try {
+    $date = new DateTime("199x-01-01");
+    echo $date->format('Y/m/d') . "\n";
+} catch (Exception $e) {
+    echo "指定された日付が不正です\n";
+    // echo "発生した例外:", $e, "\n";
+    fputs(STDERR, $e->getMessage() . "\n");
+} finally {
+    echo "end\n";
+}
+
+
+
+
+
+//10-4　意図的な例外
+
+echo "start\n";
+try {
+    throw new Exception("意図的な例外");
+    echo "例外を投げた後\n";
+} catch (Exception $e) {
+    echo "例外発生:" . $e->getMessage() . "\n";
+} finally {
+    echo "end\n";
+}
+
+
+
+//10-5　例外の種類
+
+echo "start\n";
+try {
+    throw new RangeException("意図的な例外");　//1 
+    echo "例外を投げた後\n";
+} catch (RangeException $e) {               //2
+    echo "例外発生：" . $e->getMessage() . "\n";
+} finally {
+    echo "end\n";
+}
+//1と2の○○Exeptionの種類はそろえる必要が和ある
+
+
+
+
+//10-6
+echo "start\n";
+try {
+    $pattern = 0;
+    if ($pattern == 0) {
+        throw new RangeException("意図的な範囲例外");
+    } else if ($pattern == 1) {
+        throw new LengthException("意図的な長さ例外");
+    } else {
+        throw new InvalidArgumentException("意図的なその他例外");
+    }
+    echo "例外を投げた後\n";
+} catch (RangeException $e) {                       //Range,LengthそれぞれにおいてExeptionをキャッチする
+    echo "例外発生1：" . $e->getMessage() . "\n";
+} catch (LengthException $e) {
+    echo "例外発生2：" . $e->getMessage() . "\n";
+} catch (Exception $e) {                            //Exeptionは、クラス内においてすーぱークラスとして働く。
+    echo "例外発生3：" . $e->getMessage() . "\n";
+} finally {
+    echo "end\n";
+}
+
+
+
+
+//10-7
+function test_exception($date) {
+    echo "2\n";
+    try {
+        echo "3\n";
+        return new DateTime($date);
+        echo "4\n";
+    } catch (Exception $e) {
+        echo "5\n";
+        throw $e;
+    }
+    echo "6\n";
+}
+
+echo "1\n";
+
+try {
+    $datetime = test_exception("199x-01-01");
+    echo "7\n";
+} catch (Exception $e) {
+    echo "8\n";
+}
+echo "9\n";
+
+
+
+
 
 ?>
