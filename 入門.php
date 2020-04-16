fa96327c3af4703e8cc4a42b42c4c9ff7aed41c0
“Nanami <n.ishihara@dreamcareer.co.jp>
2020-04-16 17:27:43 +0900

    6章完了

--- 入門.php
+++ 入門.php
@@ -453,5 +453,41 @@ foreach ($letter_A as $column) {
 
 
 
+//6-12
+//6-12-1
+// 2次元配列で画像を表示する
+
+// 画像用配列
+$players_img = array(
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Empty.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Dragon.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Crystal.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Hero.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Heroine.png",
+);
+
+// 配置データを読み込み
+for ($i = 0; $i < 3; $i++) {
+    $team[] = explode(",", trim(fgets(STDIN)));
+
+}
+
+// 配置データに合わせて表示する
+echo "<table>";
+foreach ($team as $row) {
+    echo "<tr>";
+    foreach ($row as $column) {
+        echo "<td><img src='".$players_img[$column]."'></td>";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+
+
+
+
+
+
 
 ?>
